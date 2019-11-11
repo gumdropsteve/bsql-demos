1f5f0afa8b721e80ba3ad67f64cb62580a2d746b
Winston <43570913+gumdropsteve@users.noreply.github.com>
2019-11-11 04:42:33 -0800

    Create colab_env.py
    reformatted; use pynvml (preinstalled in base Colab) - remove other scripts or format like so if kept

---
+++ utils/colab_env.py
@@ -0,0 +1,45 @@
+import pynvml
+
+
+def colab_env_check():
+  """
+  check the colab environment for BlazingSQL and RAPIDS AI compatiblity
+  prereqs:
+    > gpu with pascal architecture & compute capability >= 6.0
+      more info: http://bit.ly/rapids-gpus-friend
+    > colab translation: Tesla P100 or T4
+      more info: http://bit.ly/rapids_in_colab_friend_link
+  """
+  # focus GPU type (check colab settings are correct)
+  try:
+    # start up
+    pynvml.nvmlInit()
+    # identify handle then extract GPU name
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+    gpu_name = pynvml.nvmlDeviceGetName(handle)
+  # not on gpu hardware acceleration
+  except:
+    # error: hardware accelerator 
+    raise Exception("\nPlease make sure you've configured Colab to request a GPU instance type.\n\n"
+                    "At top of Colab, try: Runtime -> Change runtime type -> Hardware accelerator -> GPU -> Save\n")
+  # check if allocated K80 instance
+  if gpu_name == b'Tesla K80':
+    # error: compute capability < 6.0
+    raise Exception("\nYou've been allocated a K80 instance\n\n"
+                    "Unfortunately, colab demos require a T4 or P100 instance\n\n"
+                    "At top of Colab, try: Runtime -> Reset all runtimes...\n")
+  # compatable GPUs that have been available in Colab
+  compatable_gpus = [b'Tesla P100-PCIE-16GB', b'Tesla T4']
+  # check if allocated gpu is in list of known compatable GPUs
+  if gpu_name in compatable_gpus:
+    # we're blazing
+    return 'Woo! You got the right kind of GPU!'
+  # unacknowledged outcome
+  else:
+    # please let us know, thank you
+    raise Exception(f"\nYou've achieved wizardy.\nyour GPU is {my_gpu}\nPlease inform info@blazingsql.com")
+
+
+if __name__=='__main__':
+  # check this environment 
+  print(colab_env_check())
