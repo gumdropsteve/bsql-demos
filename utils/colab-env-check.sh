463fe6fa55486c7d09cd15698fd5fedc5ca9da45
Winston <warobson@gmail.com>
2019-11-11 18:45:58 +0000

    colab env check

--- utils/colab-env-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-wget -nc https://raw.githubusercontent.com/gumdropsteve/bsql-demos/feature_utils/utils/colab-gpu-check.py
-echo "Checking for GPU type:"
-python colab-gpu-check.py
