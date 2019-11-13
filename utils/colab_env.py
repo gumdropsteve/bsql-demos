6694e86d4fd694d7b2c591b0492a957ffa335f26
Winston <43570913+gumdropsteve@users.noreply.github.com>
2019-11-12 18:01:36 -0800

    Update colab_env.py
    testing elimination of temp fix colab install

--- utils/colab_env.py
+++ utils/colab_env.py
@@ -35,3 +35,16 @@ if gpu_name in compatable_gpus:
 else:
   # please let us know, thank you
   raise Exception(f"\nYou've achieved wizardy.\nyour GPU is {my_gpu}\nPlease inform info@blazingsql.com")
+
+# set rapids paths 
+import sys, os, time
+sys.path.append('/usr/local/lib/python3.6/site-packages/')
+os.environ['NUMBAPRO_NVVM'] = '/usr/local/cuda/nvvm/lib64/libnvvm.so'
+os.environ['NUMBAPRO_LIBDEVICE'] = '/usr/local/cuda/nvvm/libdevice/'
+
+import subprocess
+subprocess.Popen(['blazingsql-orchestrator', '9100', '8889', '127.0.0.1', '8890'],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+subprocess.Popen(['java', '-jar', '/usr/local/lib/blazingsql-algebra.jar', '-p', '8890'])
+import pyblazing.apiv2.context as cont
+cont.runRal()
+time.sleep(1) 
