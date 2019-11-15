a72592cd4b6ec6eaf8df4121fc81f60d2ab75bc7
Winston <43570913+gumdropsteve@users.noreply.github.com>
2019-11-15 14:29:24 -0800

    wget method

--- utils/bsql-colab.sh
+++ utils/bsql-colab.sh
@@ -2,66 +2,23 @@
 
 set -eu
 
-RAPIDS_VERSION="${1:-0.10}"
-
+# check for compatable GPU
 wget -nc https://github.com/gumdropsteve/bsql-demos/raw/feature/utils/utils/colab_env.py
 echo "Checking for GPU type:"
 python colab_env.py
 
-if [ ! -f Miniconda3-4.5.4-Linux-x86_64.sh ]; then
-    echo "Removing conflicting packages, will replace with RAPIDS compatible versions"
-    # remove existing xgboost and dask installs
-    pip uninstall -y xgboost dask distributed
+# install RAPIDS AI
+wget -nc https://github.com/rapidsai/notebooks-contrib/raw/master/utils/rapids-colab.sh
+bash rapids-colab.sh
 
-    # intall miniconda
-    echo "Installing conda"
-    wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
-    chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
-    bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
-
-    echo "Installing RAPIDS $RAPIDS_VERSION packages"
-    echo "Please standby, this will take a few minutes..."
-    # install RAPIDS packages
-    
-    if [ $RAPIDS_VERSION == "0.11" ] ;then
-    echo "Installing RAPIDS $RAPIDS_VERSION packages from the nightly release channel"
-    echo "Please standby, this will take a few minutes..."
-    # install RAPIDS packages
-        conda install -y --prefix /usr/local \
-                -c rapidsai-nightly/label/xgboost -c rapidsai-nightly -c nvidia -c conda-forge \
-                python=3.6 cudatoolkit=10.1 \
-                cudf=$RAPIDS_VERSION cuml cugraph gcsfs pynvml \
-                dask-cudf \
-                xgboost
-    else
-        echo "Installing RAPIDS $RAPIDS_VERSION packages from the stable release channel"
-        echo "Please standby, this will take a few minutes..."
-        # install RAPIDS packages
-        conda install -y --prefix /usr/local \
-            -c rapidsai/label/xgboost -c rapidsai -c nvidia -c conda-forge \
-            python=3.6 cudatoolkit=10.1 \
-            cudf=$RAPIDS_VERSION cuml cugraph gcsfs pynvml \
-            dask-cudf \
-            xgboost
-    
-    fi
-
-    # install BlazingSQL
-    echo "Installing BlazingSQL"
-    echo "Please standby, this may take a few minutes..."
-    conda install -y --prefix /usr/local -c blazingsql/label/cuda10.0 -c blazingsql -c rapidsai -c conda-forge -c defaults blazingsql-calcite blazingsql-orchestrator blazingsql-ral blazingsql-python
-    pip install flatbuffers
-    
-    fi
-      
-    echo "Copying shared object files to /usr/lib"
-    # copy .so files to /usr/lib, where Colab's Python looks for libs
-    cp /usr/local/lib/libcudf.so /usr/lib/libcudf.so
-    cp /usr/local/lib/librmm.so /usr/lib/librmm.so
-    cp /usr/local/lib/libnccl.so /usr/lib/libnccl.so
+# install BlazingSQL
+echo "Installing BlazingSQL"
+echo "Please standby, this may take a few minutes..."
+conda install -y --prefix /usr/local -c blazingsql/label/cuda10.0 -c blazingsql -c rapidsai -c conda-forge -c defaults blazingsql-calcite blazingsql-orchestrator blazingsql-ral blazingsql-python
+pip install flatbuffers
 fi
 
 echo ""
-echo "*********************************************"
+echo "********************************************************"
 echo "Your Google Colab instance is BlazingSQL + RAPIDS ready!"
-echo "*********************************************"
+echo "********************************************************"
