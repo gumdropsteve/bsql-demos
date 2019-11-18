662788b00f16fb7668c09ff1144dd60b8bd4c370
Winston <43570913+gumdropsteve@users.noreply.github.com>
2019-11-18 08:39:30 -0800

    Update bsql-colab.sh
    add output feedback for user

--- utils/bsql-colab.sh
+++ utils/bsql-colab.sh
@@ -1,3 +1,11 @@
+echo "Installing BlazingSQL from the stable release channel"
+echo "Please standby, this will take a few minutes..."
+
 conda install -y --prefix /usr/local -c blazingsql/label/cuda10.0 -c blazingsql -c rapidsai -c conda-forge -c defaults blazingsql-calcite blazingsql-orchestrator blazingsql-ral blazingsql-python
 
 pip install flatbuffers
+
+echo ""
+echo "***********************************************"
+echo "Your Google Colab instance is BlazingSQL ready!"
+echo "***********************************************"
