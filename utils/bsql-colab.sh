e69e2cda0664396e77922a0db3d5477018942b67
Winston <43570913+gumdropsteve@users.noreply.github.com>
2019-11-18 08:43:48 -0800

    add spacing

--- utils/bsql-colab.sh
+++ utils/bsql-colab.sh
@@ -1,5 +1,7 @@
+echo ""
 echo "Installing BlazingSQL from the stable release channel"
 echo "Please standby, this will take a few minutes..."
+echo ""
 
 conda install -y --prefix /usr/local -c blazingsql/label/cuda10.0 -c blazingsql -c rapidsai -c conda-forge -c defaults blazingsql-calcite blazingsql-orchestrator blazingsql-ral blazingsql-python
 
