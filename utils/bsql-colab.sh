3b6728e90ec5fa8ca18c11a0e5f905551a480115
Winston <43570913+gumdropsteve@users.noreply.github.com>
2019-11-18 10:26:07 -0800

    add cuda toolkit

--- utils/bsql-colab.sh
+++ utils/bsql-colab.sh
@@ -3,7 +3,7 @@ echo "Installing BlazingSQL from the stable release channel"
 echo "Please standby, this will take a few minutes..."
 echo ""
 
-conda install -y --prefix /usr/local -c blazingsql/label/cuda10.0 -c blazingsql -c rapidsai -c conda-forge -c defaults blazingsql-calcite blazingsql-orchestrator blazingsql-ral blazingsql-python
+conda install -y --prefix /usr/local -c blazingsql/label/cuda10.0 -c blazingsql -c rapidsai -c conda-forge -c defaults blazingsql-calcite blazingsql-orchestrator blazingsql-ral blazingsql-python cudatoolkit=10.0
 
 pip install flatbuffers
 
